ae87b78401d9429d18c6b53b6115f6d63adfbd8b
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2022-09-14 21:04:45 +0200

    add some scripts

--- js/script.js
+++ js/script.js
@@ -16,23 +16,54 @@ let typed = new Typed('.typing', {
 const nav = document.querySelector('.nav');
 const navList = nav.querySelectorAll('li');
 const totalNavList = navList.length;
+const allSections = document.querySelectorAll('.section');
+const totalSections = allSections.length;
 
 for(let i=0; i<totalNavList; i++) {
     const a = navList[i].querySelector('a');
-    a.addEventListener('click', ()=>{
+    a.addEventListener('click', function(){
+
+        for(let i=0; i<totalSections; i++) {
+            allSections[i].classList.remove('back-section');
+        }
+
         for (let j=0; j<totalNavList; j++) {
+            
+            if ( navList[j].querySelector('a').classList.contains('active') ) {
+                allSections[j].classList.add('back-section')
+            }
             navList[j].querySelector('a').classList.remove('active')
         }
+        // console.log(this);
         this.classList.add('active')
+        showSection(this);
     })
 }
 
+function showSection(element) {
+    for(let i=0; i<totalSections; i++) {
+        allSections[i].classList.remove('active');
+    }
+    const target = element.getAttribute('href').split('#')[1];
+    console.log(target);
+    document.querySelector('#' + target).classList.add('active')
+}
 
+// Nav Toggler Button 
+const navTogglerBtn = document.querySelector('.nav-toggle')
+const aside = document.querySelector('aside');
 
+navTogglerBtn.addEventListener('click', ()=>{
+    asideSectionTogglerBtn();
+})
 
-
-
-
+function asideSectionTogglerBtn() {
+    aside.classList.toggle('open');
+    navTogglerBtn.classList.toggle('open');
+    for(let i=0; i<totalSections; i++){
+        allSections[i].classList.toggle('open'); 
+    }
+}
 
 
 // End Aside
