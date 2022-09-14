f5f432d74e277cb673c75baccaeb3ba16142ad86
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2022-09-15 00:12:13 +0200

    new functions

--- js/script.js
+++ js/script.js
@@ -23,31 +23,64 @@ for(let i=0; i<totalNavList; i++) {
     const a = navList[i].querySelector('a');
     a.addEventListener('click', function(){
 
-        for(let i=0; i<totalSections; i++) {
-            allSections[i].classList.remove('back-section');
-        }
+        removeBackSection();
 
         for (let j=0; j<totalNavList; j++) {
             
             if ( navList[j].querySelector('a').classList.contains('active') ) {
-                allSections[j].classList.add('back-section')
+                // allSections[j].classList.add('back-section')
+                addBackSection(j);
+
             }
             navList[j].querySelector('a').classList.remove('active')
         }
         // console.log(this);
         this.classList.add('active')
         showSection(this);
+        if (window.innerWidth < 1200)
+            asideSectionTogglerBtn()
     })
 }
-
+function removeBackSection() {
+    for(let i=0; i<totalSections; i++) {
+        allSections[i].classList.remove('back-section');
+    }
+}
+function addBackSection(params) {
+    allSections[params].classList.add('back-section')
+}
 function showSection(element) {
     for(let i=0; i<totalSections; i++) {
         allSections[i].classList.remove('active');
     }
     const target = element.getAttribute('href').split('#')[1];
-    console.log(target);
+    // console.log(target);
     document.querySelector('#' + target).classList.add('active')
+    console.log('show section');
 }
+function updateNav(element) {
+    for (let i=0; i<totalNavList; i++) {
+        navList[i].querySelector('a').classList.remove('active');
+        const target = element.getAttribute('href').split('#')[1]
+        if (target === navList[i].querySelector('a').getAttribute('href').split('#')[1]) {
+            navList[i].querySelector('a').classList.add('active');
+        }
+    }
+    console.log('update Nav');
+}
+
+
+// Click on hire me
+
+document.querySelector('.hire-me').addEventListener('click', function(){;
+    const sectionIndex = this.getAttribute('data-section-index');
+    // console.log(sectionIndex);
+    // console.log('button clicked');
+    showSection(this);
+    updateNav(this)
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
 
 // Nav Toggler Button 
 const navTogglerBtn = document.querySelector('.nav-toggle')
